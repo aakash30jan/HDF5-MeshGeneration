78481de97b7c4e3937b8e34c07640954c097144a
aero <aakash30jan@gmail.com>
2017-10-22 17:56:36 +0200

    Initial commit

--- push2git.sh
+++
@@ -1,7 +0,0 @@
-cd ~/workdir/ClassWork-TurbulenceMaster/
-git init
-git remote add origin git@github.com:aakash30jan/ClassWork-TurbulenceMaster.git
-git add --all .
-git commit -m 'Reinitialized Files'
-git pull origin master
-git push origin master
--- pushproject.sh
+++
@@ -1,9 +0,0 @@
-#cd ~/workdir/MeshGeneration-Framework/
-git init
-git remote add origin git@github.com:aakash30jan/MeshGeneration-Framework.git
-git add --all .
-git commit -m 'Initial commit'
-git pull origin master
-git push origin master
-
-
